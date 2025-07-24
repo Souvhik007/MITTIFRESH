17e86502ab469d999449568b385259e47b0827bc
Souvhik Goswami <112115857+Souvhik007@users.noreply.github.com>
2025-07-24 13:06:35 +0530

    Update Categories.jsx

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -11,12 +11,6 @@ const Categories = () => {
         <p className='text-2xl md:text-3xl font-medium'>Categories</p>
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6'>
 
-
-           {categories.map((category, index)=>(
-
-          <div key={index} className='group cusor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center'
-
-            style={{backgroundColor: category.bgColor}}
             onClick={()=>{
                navigate( `/products/${category.path.toLowerCase()}`);
                scrollTo(0,0)
