adb7085b0be714d054fb08bd761196bfbd928dd7
Souvhik Goswami <112115857+Souvhik007@users.noreply.github.com>
2025-09-11 23:07:39 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,12 @@
+import { createContext, useContext, useEffect, useState} from "react";
+import {useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children})=>{
 
      const  currency = import.meta.VITE_CURRENCY;
 
